41bbabeb84122c1b870f514eb7e077bf0f76b938
aeryshev <a.a.eryshev@gmail.com>
2017-05-26 16:50:01 +0200

    Double language layout.

--- .config/i3/scripts/switch_layouts.sh
+++ .config/i3/scripts/switch_layouts.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
-layouts=(fr ru us)
 current_layout=$(~/.config/i3/tools/xkblayout-state/xkblayout-state print %s | awk '{print $0}')
 layout_to_set="fr"
-if [ "$current_layout" == "fr" ]; then
-    layout_to_set="ru"
-elif [ "$current_layout" == "ru" ]; then
-    layout_to_set="us"
+# if [ "$current_layout" == "fr" ]; then
+#     layout_to_set="ru"
+# elif [ "$current_layout" == "ru" ]; then
+# elif [ "$current_layout" == "ru" ]; then
+#     layout_to_set="us -variant intl"
+# elif [ "$current_layout" == "us" ]; then
+#     layout_to_set="fr"
+# fi
+if [ "$current_layout" == "ru" ]; then
+    layout_to_set="us -variant intl"
 elif [ "$current_layout" == "us" ]; then
-    layout_to_set="fr"
+    layout_to_set="ru"
 fi
 echo $layout_to_set
 setxkbmap -model pc105 -layout $layout_to_set
