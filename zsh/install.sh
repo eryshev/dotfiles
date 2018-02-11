92e4850b7343d83d8fb9d0f583f1e7ca918f82a8
Alexey ERYSHEV <a.a.eryshev@gmail.com>
2018-02-11 22:49:34 +0100

    adjustements of zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 if [ -f ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     rm ~/.zshrc
     mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+    ZSH_CUSTOM="../.oh-my-zsh/custom"
+    cd $ZSH_CUSTOM/plugins
+    git clone https://github.com/zsh-users/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting
+    cd -
 fi
-ZSH_CUSTOM="../.oh-my-zsh/custom"
-cd $ZSH_CUSTOM/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting
-cd -
