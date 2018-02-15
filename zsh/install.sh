a3445bbf126c5e3a14251112c272999fc9c44f12
Alexey Eryshev <a.eryshev@criteo.com>
2018-02-15 17:29:21 +0100

    correct install zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-if [ -f ~/.oh-my-zsh ]; then
+if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     rm ~/.zshrc
     mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
-    ZSH_CUSTOM="../.oh-my-zsh/custom"
-    cd $ZSH_CUSTOM/plugins
+    cd ~/.oh-my-zsh/custom/plugins
     git clone https://github.com/zsh-users/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting
     cd -
