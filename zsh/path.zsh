ea4dfeb0273fb8c6b5bfa518841976af28a8b483
Alexey Eryshev <a.eryshev@criteo.com>
2018-02-16 18:19:58 +0100

    add maven to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,5 @@
 export PATH=~/.dotfiles/bin:$PATH
 export PATH=~/app/sbt/bin:$PATH
+export PATH=~/app/node-v6.11.3-linux-x64/bin/:$PATH
+export M2_HOME=/opt/apache-maven-3.5.0/
+export PATH=$PATH:$M2_HOME/bin
