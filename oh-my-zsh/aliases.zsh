e9f05052440a5fab809484556f381c3c7c6d2af0
Alexey ERYSHEV <a.a.eryshev@gmail.com>
2018-02-21 14:02:48 +0100

    update

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -3,5 +3,11 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias ger='gp origin HEAD:refs/publish/master'
+
 alias gu='ga -A && gc -am "update" && gp'
 
+alias mvn="mvn -T4"
+alias mvni="mvn install -DskipTests"
+alias mvnrm="mvn bfs:refresh-moab"
+alias mvnrs="mvn bfs:refresh-sources"
+alias mvnr="mvnrm && mvnrs && mvni"
