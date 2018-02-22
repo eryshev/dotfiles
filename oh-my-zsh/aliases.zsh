64c5ed17fc98f3cb32b9833b32ca22a01c6ec720
Alexey Eryshev <a.eryshev@criteo.com>
2018-02-22 10:41:03 +0100

    update

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -4,10 +4,12 @@ alias cls='clear' # Good 'ol Clear Screen command
 
 alias ger='gp origin HEAD:refs/publish/master'
 
-alias gu='ga -A && gc -am "update" && gp'
+alias dp='ga -A && gc -am "update" && gp'
 
 alias mvn="mvn -T4"
 alias mvni="mvn install -DskipTests"
 alias mvnrm="mvn bfs:refresh-moab"
 alias mvnrs="mvn bfs:refresh-sources"
 alias mvnr="mvnrm && mvnrs && mvni"
+
+alias dl="cd ~/.dotfiles && gl && cd -"
