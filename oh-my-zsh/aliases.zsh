648ee524c2a6bf56839a50eb153cc7167faadc7b
Alexey ERYSHEV <a.a.eryshev@gmail.com>
2018-03-02 15:08:24 +0100

    add mvnd alias

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -6,6 +6,7 @@ alias ger='gp origin HEAD:refs/publish/master'
 
 alias gu='ga -A && gc -am "update" && gp'
 
+alias mvnd="mvn -DdevMode"
 alias mvn="mvn -T4"
 alias mvni="mvn install -DskipTests"
 alias mvnrm="mvn bfs:refresh-moab"
