552cb9baa447fce78d4b0d78991cc552bcaea397
Alexey Eryshev <a.eryshev@criteo.com>
2018-04-03 15:23:10 +0200

    anaconda as a python distribution

--- oh-my-zsh/path.zsh
+++ oh-my-zsh/path.zsh
@@ -4,3 +4,4 @@ export PATH=~/app/node-v6.11.3-linux-x64/bin/:$PATH
 export M2_HOME=/opt/apache-maven-3.5.0/
 export PATH=$PATH:$M2_HOME/bin
 export PATH=$HOME/app:$PATH
+export PATH=$HOME/app/anaconda3/bin:$PATH
