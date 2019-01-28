9f29567492d9e25a1a52f8e7eeceb90911a64da3
Alexey ERYSHEV <aeryshev@a.eryshev>
2019-01-28 10:11:40 +0100

    mix osx and linux config

---
+++ i3/scripts/individual_scrot.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+xdpyinfo -ext XINERAMA | sed '/^  head #/!d;s///' |
+while IFS=' :x@,' read i w h x y; do
+    import -window root -crop ${w}x$h+$x+$y /tmp/head_$i.png
+done
--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -15,5 +15,6 @@ alias mvnr="mvnrm && mvnrs && mvni"
 
 alias dl="cd ~/.dotfiles && gl && cd -"
 
-alias vi="/usr/local/Cellar/vim/8.1.0202/bin/vim"
+# alias vi="/usr/local/Cellar/vim/8.1.0202/bin/vim"
+
 
