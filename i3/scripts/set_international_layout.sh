b9df5f4c8980fb8bee5dc690079a2da4fdc15e71
Alexey ERYSHEV <a.eryshev@criteo.com>
2019-05-29 11:10:36 +0200

    add layout switch and also replace Mod1 by Mod4 for i3

--- i3/scripts/set_international_layout.sh
+++ i3/scripts/set_international_layout.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 current_layout=$(~/.config/i3/tools/xkblayout-state/xkblayout-state print %s | awk '{print $0}')
-# layout_to_set="fr"
-# if [ "$current_layout" == "fr" ]; then
-#     layout_to_set="ru"
-# elif [ "$current_layout" == "ru" ]; then
-# elif [ "$current_layout" == "ru" ]; then
-#     layout_to_set="us -variant intl"
-# elif [ "$current_layout" == "us" ]; then
-#     layout_to_set="fr"
-# fi
 layout_to_set="us"
 if [ "$current_layout" == "ru" ]; then
-    layout_to_set="us"
+    layout_to_set="us -variant intl"
 elif [ "$current_layout" == "us" ]; then
     layout_to_set="ru"
 fi
