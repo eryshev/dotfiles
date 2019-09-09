f94c3ced511e9643c426823f98f64656c8deb8a9
Alexey ERYSHEV <a.a.eryshev@gmail.com>
2019-09-09 14:46:11 +0200

    Bootstrap omf and zsh plugin

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,6 @@
+# mix emacs and vim keybindings
 fish_hybrid_key_bindings
+
+# FIXME this plugin from omf doesn't get sourced
+source ~/.local/share/omf/pkg/ssh_agent/ssh_agent.fish
+test_identities
