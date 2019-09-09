413a98a0e8fe70741a3f34ecb47f9725e04e7a29
Alexey ERYSHEV <a.a.eryshev@gmail.com>
2019-09-09 12:57:28 +0200

    Use standart fish function for hybrid bindings

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,1 @@
-function fish_user_key_bindings
-    # Execute this once per mode that emacs bindings should be used in
-    fish_default_key_bindings -M insert
-    # Without an argument, fish_vi_key_bindings will default to
-    # resetting all bindings.
-    # The argument specifies the initial mode (insert, "default" or visual).
-    fish_vi_key_bindings --no-erase insert
-end
-
-fish_user_key_bindings
-
+fish_hybrid_key_bindings
