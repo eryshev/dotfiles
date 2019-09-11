1bf0ba44156ea48bbe4d8d1d50f59b39d70efdf1
Alexey ERYSHEV <a.a.eryshev@gmail.com>
2019-09-11 13:18:37 +0200

    Use my own fork for ssh_agent fzf key bindings

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
 # mix emacs and vim keybindings
 fish_hybrid_key_bindings
 
-# FIXME this plugin from omf doesn't get sourced
-source ~/.local/share/omf/pkg/ssh_agent/ssh_agent.fish
-test_identities
+# add fzf keybindings
+fzf_key_bindings
