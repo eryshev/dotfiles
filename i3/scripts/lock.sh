5533fce345c198f8b2264de37c71316712f6a09a
Alexey ERYSHEV <a.a.eryshev@gmail.com>
2019-09-13 16:24:04 +0200

    Fix i3 lock script

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -10,11 +10,11 @@ convert $TEMPDIR/head_0.png -scale 10% -scale 1000% $TEMPDIR/head_0.png
 if [ -e $TEMPDIR/head_1.png ]; then
   convert $TEMPDIR/head_1.png -scale 10% -scale 1000% $TEMPDIR/head_1.png
   [[ -f $ICON ]] && convert $TEMPDIR/head_1.png $ICON -gravity center -composite -matte $TEMPDIR/head_1.png
-  convert $TEMPDIR/head_0.png $TEMPDIR/head_1.png +append $TEMPDIR/screen.png
+  convert $TEMPDIR/head_1.png $TEMPDIR/head_0.png +append $TEMPDIR/screen.png
 else
   mv $TEMPDIR/head_0.png $TEMPDIR/screen.png
 fi
 
 
 i3lock -i $TEMPDIR/screen.png
-rm $TEMPDIR/*.png
+rm $TEMPDIR/screen.png $TEMPDIR/head_*.png
