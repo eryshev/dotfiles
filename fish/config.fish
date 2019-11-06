a82b61671835a991b39e4f07e1c465ea8faedbb4
eryshev <a.a.eryshev@gmail.com>
2019-11-05 23:16:06 -0800

    Config bob the fis

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ fish_hybrid_key_bindings
 # add fzf keybindings
 fzf_key_bindings
 
-set -g theme_powerline_fonts no
+set -g theme_date_format +"%d-%m-%Y %H:%M:%S"
 set -g theme_display_git_dirty no
 set -g theme_display_git_untracked no
 set -g theme_display_git_ahead_verbose no 
