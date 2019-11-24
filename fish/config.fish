c4ac35fea5aa60dd5a4f0cb26f0f70b01813fe8f
eryshev <a.a.eryshev@gmail.com>
2019-11-24 21:36:50 +0100

    update

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,7 @@ fish_hybrid_key_bindings
 # add fzf keybindings
 fzf_key_bindings
 
+# configure bobthefish for huge git repos
 set -g theme_date_format +"%d-%m-%Y %H:%M:%S"
 set -g theme_display_git_dirty no
 set -g theme_display_git_untracked no
@@ -14,6 +15,9 @@ set -g theme_display_git_ahead_verbose no
 set -g theme_display_git_dirty_verbose no
 set -g theme_display_git_stashed_verbose no
 set -g theme_display_git_master_branch yes
+
+# abbr
+abbr -a gu git add -A && git commit -a -m 'update' && git push
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
