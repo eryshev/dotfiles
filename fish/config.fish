1d955db2a5c56e5a1b7dbda1c09f3c0cd5c09582
eryshev <a.a.eryshev@gmail.com>
2020-12-07 15:18:19 +0100

    comment out unused bits of fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,24 @@
 # proprietary config
-proprietary_config
+# proprietary_config
 
 # mix emacs and vim keybindings
 fish_hybrid_key_bindings
 
 # add fzf keybindings
-fzf_key_bindings
+# fzf_key_bindings
 
 set -g theme_date_format +"%d-%m-%Y %H:%M:%S"
 set -g theme_display_date no
 # configure bobthefish for huge git repos
 set -g theme_display_git_dirty no
 set -g theme_display_git_untracked no
-set -g theme_display_git_ahead_verbose no 
+set -g theme_display_git_ahead_verbose no
 set -g theme_display_git_dirty_verbose no
 set -g theme_display_git_stashed_verbose no
 set -g theme_display_git_master_branch no
 # disable renv and venv strings
 set -g theme_display_ruby yes
-set -g theme_display_virtualenv yes 
+set -g theme_display_virtualenv yes
 # cursor on a new line
 set -g theme_newline_cursor yes
 
@@ -49,11 +49,11 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+# eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 # rbenv
-status --is-interactive; and source (rbenv init -|psub)
+# status --is-interactive; and source (rbenv init -|psub)
 
 # Rust
 set PATH $HOME/.cargo/bin $PATH
